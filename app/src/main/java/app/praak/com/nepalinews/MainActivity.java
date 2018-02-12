9cb45973c4f8a4d47bcc072b9bab6a4c7998f5b3
praak <praakrit@gmail.com>
2018-02-12 15:44:39 -0500

    Initial commit

---
+++ app/src/main/java/app/praak/com/nepalinews/MainActivity.java
@@ -0,0 +1,15 @@
+package app.praak.com.nepalinews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // new test.
+}
